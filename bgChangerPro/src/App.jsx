2ba934a094edc54ca1a28f4aa2b505124cba67b5
selfmanish01 <selfmanish01@gmail.com>
2024-01-05 19:11:18 +0530

    full function app bgChangerPro

---
+++ bgChangerPro/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+function App() {
+  const [color, setColor] = useState("Olive");
+
+  const handleColorChange = (newColor) => {
+    setColor(newColor);
+  };
+
+  return (
+    <div className="w-full h-screen duration-200 " style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => handleColorChange("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => handleColorChange("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => handleColorChange("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => handleColorChange("pink")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
